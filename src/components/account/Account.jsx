78bb06d77c7d5cf720e71680dafbd16bcb3b7cbf
Yuusuke <masngelox7@gmail.com>
2022-05-14 14:47:19 +0200

    Create User page

---
+++ src/components/account/Account.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Account.css";
+import PropTypes from 'prop-types';
+
+export default function Account(props){
+    return(
+        <section className="account">
+            <div className="account-content-wrapper">
+                <h3 className="account-title">{props.title}</h3>
+                <p className="account-amount">{props.amount}</p>
+                <p className="account-amount-description">{props.description}</p>
+            </div>
+            <div className="account-content-wrapper cta">
+                <button className="transaction-button">View transaction</button>
+            </div>
+        </section>
+    );
+}
+
+Account.prototype = {
+    title: PropTypes.string,
+    amount: PropTypes.string,
+    description: PropTypes.string
+}
